63be666eb35e0a4d34021b6df0551bc0f3697a9f
Le Tran Anh Vu <vule5726@gmail.com>
2024-05-11 14:36:15 +0300

    fix decimal returned as string in knex

--- src/db/db-context.ts
+++ src/db/db-context.ts
@@ -1,6 +1,10 @@
 import knex from 'knex'
 import dbConfig from './db-config'
+import pg from 'pg'
 
+// Use to convert the decimal value to number otherwise it will be a string.
+// Github Issue: https://github.com/knex/knex/issues/927#issuecomment-1011920890
+pg.types.setTypeParser(pg.types.builtins.NUMERIC, Number)
 const dbContext = knex(dbConfig)
 
 export default dbContext
