de25b43543dce05469c0f74348498691dce38656
Le Tran Anh Vu <vule5726@gmail.com>
2024-05-11 13:00:48 +0300

    remove objection js

--- src/db/db-config.ts
+++ src/db/db-config.ts
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv'
+import type { Knex } from 'knex'
 import path from 'node:path'
-import { knexSnakeCaseMappers } from 'objection'
 
 dotenv.configDotenv({ path: path.join(__dirname, '../../.env') })
 
 const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PW } = process.env
+
 export default {
     client: 'pg',
     connection: {
@@ -14,5 +15,40 @@ export default {
         password: DB_PW,
         database: DB_NAME,
     },
-    ...knexSnakeCaseMappers(),
+    wrapIdentifier: (value, origImpl, queryContext) => origImpl(convertToSnakeCase(value)),
+    postProcessResponse: (result, queryContext): any => {
+        let newResult = null
+        if (Array.isArray(result)) {
+            newResult = result.map(convertKeyToCamelCase)
+        } else {
+            newResult = convertKeyToCamelCase(result)
+        }
+
+        return newResult
+    },
+} as Knex.Config
+
+function convertToSnakeCase(value: string): string {
+    return value.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`)
+}
+
+function convertToCamelCase(value: string): string {
+    return value.replace(/_[a-z]/g, (_firstLetter) => _firstLetter.substring(1).toUpperCase())
+}
+
+function isSnakeCase(key: string): boolean {
+    return key.includes('_')
+}
+
+function convertKeyToCamelCase(obj: Record<string, any>): Record<string, any> {
+    const clone = { ...obj }
+    for (const key in clone) {
+        if (isSnakeCase(key)) {
+            const newKey = convertToCamelCase(key)
+            clone[newKey] = clone[key]
+            delete clone[key]
+        }
+    }
+
+    return clone
 }
